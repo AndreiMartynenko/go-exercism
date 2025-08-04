9521925000f5a729dd320095527dd2df5cbf6157
AndrewLondon <andrewmart@mail.com>
2025-08-04 18:47:13 +0100

    purchase

---
+++ license/main.go
@@ -0,0 +1,29 @@
+package license
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle.
+// Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection.
+// It returns the vehicle that comes first in lexicographical order, with a message.
+func ChooseVehicle(option1, option2 string) string {
+	var best string
+	if option1 < option2 {
+		best = option1
+	} else {
+		best = option2
+	}
+	return best + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age < 10 {
+		return originalPrice * 0.7
+	}
+	return originalPrice * 0.5
+}
