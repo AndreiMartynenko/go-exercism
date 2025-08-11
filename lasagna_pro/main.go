000b088f3322cd04ed4537d8884cba079e62ca2b
AndrewLondon <andrewmart@mail.com>
2025-08-11 23:58:41 +0100

    lasagna pro

--- lasagna_pro/main.go
+++ lasagna_pro/main.go
@@ -1,19 +1,16 @@
-package lasagnapro
+package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime calculates the total prep time.
+// If timePerLayer == 0, default to 2 minutes per layer.
 func PreparationTime(layers []string, timePerLayer int) int {
-	// This function should return the preparation time in minutes
-	// based on the number of layers and the time per layer.
-	// For now, it just panics to indicate it's not implemented.
-
-	//by default, if no timePerLayer is provided, it should be 2 minutes
 	if timePerLayer == 0 {
 		timePerLayer = 2
 	}
 	return len(layers) * timePerLayer
 }
 
-// TODO: define the 'Quantities()' function
+// Quantities computes the amounts needed based on layers.
+// Noodles: +50g per "noodles"; Sauce: +0.2l per "sauce".
 func Quantities(layers []string) (int, float64) {
 	noodles := 0
 	sauce := 0.0
@@ -27,25 +24,21 @@ func Quantities(layers []string) (int, float64) {
 	return noodles, sauce
 }
 
-// TODO: define the 'AddSecretIngredient()' function
-func AddSecretIngredient(listOfFriend, ingredientList []string) {
-	if len(listOfFriend) == 0 || len(ingredientList) == 0 {
+// AddSecretIngredient replaces the last item in your ingredientList
+// with your friend's last item (their secret ingredient), in place.
+func AddSecretIngredient(friendList, ingredientList []string) {
+	if len(friendList) == 0 || len(ingredientList) == 0 {
 		return
 	}
-	// The secret ingredient is the last one in the friend's list
-	secretIngredient := listOfFriend[len(listOfFriend)-1]
-	ingredientList[len(ingredientList)-1] = secretIngredient
+	ingredientList[len(ingredientList)-1] = friendList[len(friendList)-1]
 }
 
-// TODO: define the 'ScaleRecipe()' function
-
-func ScaleRecipe() {}
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+// ScaleRecipe returns a NEW slice with quantities scaled from a base of 2 portions.
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaled := make([]float64, len(quantities))
+	factor := float64(portions) / 2.0
+	for i, q := range quantities {
+		scaled[i] = q * factor
+	}
+	return scaled
+}
