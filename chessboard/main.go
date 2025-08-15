9b24b254318ea37138b25a5037331a8392d2cec2
AndrewLondon <andrewmart@mail.com>
2025-08-15 13:58:41 +0100

    chessboard

--- chessboard/main.go
+++ chessboard/main.go
@@ -10,21 +10,48 @@ type Chessboard map[string]File
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
 func CountInFile(cb Chessboard, file string) int {
-
+	if f, ok := cb[file]; ok {
+		count := 0
+		for _, occupied := range f {
+			if occupied {
+				count++
+			}
+		}
+		return count
+	}
+	return 0
 }
 
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank.
 func CountInRank(cb Chessboard, rank int) int {
-
+	count := 0
+	for _, file := range cb {
+		if rank < len(file) && file[rank] {
+			count++
+		}
+	}
+	return count
 }
 
 // CountAll should count how many squares are present in the chessboard.
 func CountAll(cb Chessboard) int {
-
+	count := 0
+	for _, file := range cb {
+		count += len(file)
+	}
+	return count
 }
 
 // CountOccupied returns how many squares are occupied in the chessboard.
 func CountOccupied(cb Chessboard) int {
-
+	count := 0
+	for _, file := range cb {
+		for _, occupied := range file {
+			if occupied {
+				count++
+			}
+		}
+	}
+	return count
 }
