8610bdf28f43118f2e3f57c9605ed17fb5b62282
AndrewLondon <andrewmart@mail.com>
2025-08-24 17:46:56 +0100

    raindrops

---
+++ raindrops/main.go
@@ -0,0 +1,47 @@
+package raindrops
+
+import "fmt"
+
+// func Convert(number int) string {
+// 	if number%3 == 0 && number%5 == 0 && number%7 == 0 {
+// 		return "PlingPlangPlong"
+// 	}
+// 	if number%3 == 0 && number%5 == 0 {
+// 		return "PlingPlang"
+// 	}
+// 	if number%3 == 0 && number%7 == 0 {
+// 		return "PlingPlong"
+// 	}
+// 	if number%5 == 0 && number%7 == 0 {
+// 		return "PlangPlong"
+// 	}
+// 	if number%3 == 0 {
+// 		return "Pling"
+// 	}
+// 	if number%5 == 0 {
+// 		return "Plang"
+// 	}
+// 	if number%7 == 0 {
+// 		return "Plong"
+// 	}
+// 	return fmt.Sprintf("%d", number)
+// }
+
+func Convert(number int) string {
+	result := ""
+
+	if number%3 == 0 {
+		result += "Pling"
+	}
+	if number%5 == 0 {
+		result += "Plang"
+	}
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		return fmt.Sprintf("%d", number)
+	}
+	return result
+}
